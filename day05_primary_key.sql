8eabe7ea02540aa45fc805b4df29b68558d20c72
Berke <93223660+quatronostro@users.noreply.github.com>
2023-04-19 20:22:17 +0300

    day06

--- day05_primary_key.sql
+++ day05_primary_key.sql
@@ -73,9 +73,85 @@ CREATE TABLE universite_ogrenci_bilgi_sistemi
 (
 giris_yili INT,
 giris_siralamasi INT,
-isim VARCHAR(20) NOT NULL,
+isim VARCHAR(25) NOT NULL,
 bolum VARCHAR(15),
 CONSTRAINT id_girisyil_girissira PRIMARY KEY (giris_yili, giris_siralamasi)
+);
+
+
+/*====================================== FOREIGN KEY ==============================================
+=> Foreign Key iki tablo arasinda Relation olusturmak icin kullanilir. 
+=> Foreign Key baska bir tablonun Primary Key'ine baglidir. 
+=> Referenced table (baglanilan tablo, Primary Key'in oldugu Tablo) parent table olarak adlandirilir. 
+   Foreign Key'in oldugu tablo ise child table olarak adlandirilir. 
+=> Bir Tabloda birden fazla Foreign Key olabilir. 
+=> Foreign Key NULL degeri alabilir. 
+=> Foreign Key duplicate olabilir. 
+Note 1: "Parent Table"da olmayan bir id'ye sahip datayi "Child Table"'a ekleyemezsiniz .
+Note 2: Child Table'i silmeden Parent Table'i silemezsiniz. Once "Child Table" silinir, sonra "Parent Table" silinir.
+        
+-----Syntax-----
+CONSTRAINT constraintName FOREIGN KEY(table_name_id) 
+REFERENCES parent_table (parent_table_field_name) 
+CREATE TABLE table_name
+(
+id char(10),
+name char(10),
+CONSTRAINT constraint_name 
+FOREIGN KEY (table_name_id) 
+REFERENCES parent_table (parent_table_field_name) 
+);
+        
+==================================================================================================*/
+
+/*------------------------------------------------------------------------
+"tedarikci" isimli bir tablo olusturun. 
+Tabloda "tedarikci_id", "tedarikci_ismi", "iletisim_isim" field'lari olsun 
+ve "tedarikci_id" yi Primary Key yapin.
+ "urunler" isminde baska bir tablo olusturun "tedarikci_id" ve "urun_id"
+ field'lari olsun ve "tedarikci_id" yi Foreign Key yapin.
+---------------------------------------------------------------------------*/
+
+ CREATE TABLE tedarikci 
+(
+ tedarikci_id INT,
+ tedarikci_ismi VARCHAR(20),
+ iletisim_isim VARCHAR(30),
+ CONSTRAINT id_pk PRIMARY KEY (tedarikci_id)
+);
+
+ CREATE TABLE urunler 
+(
+ ted_id INT,
+ urun_id CHAR(13),
+CONSTRAINT urunler_fk FOREIGN KEY (ted_id) 
+REFERENCES tedarikci (tedarikci_id) 
+);
+
+
+/*------------------------------------------------------------------------
+"bebeler" isimli bir tablo olusturun. 
+Tabloda "id", "isim", "iletisim" field'lari olsun 
+ve "id" yi Primary Key yapin.
+ "notlar" isminde baska bir tablo olusturun "bebe_id" ve "puan"
+ field'lari olsun ve "bebe_id" yi Foreign Key yapin.
+---------------------------------------------------------------------------*/
+
+
+ CREATE TABLE bebeler 
+(
+ id INT,
+ isim VARCHAR(20),
+ iletisim VARCHAR(30),
+ CONSTRAINT id_pk PRIMARY KEY (id)
+);
+
+ CREATE TABLE notlar 
+(
+ bebe_id INT,
+ puan CHAR(5),
+CONSTRAINT bebe_fk FOREIGN KEY (bebe_id) 
+REFERENCES bebeler (id) 
 );
 
 
